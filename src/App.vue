4d48bd61e3cb79f08ed1292dce974d9b0854fffc
Jose <j.estrada@oh.marketing>
2018-06-18 18:09:15 -0500

    up

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+   <h1>Hola</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '/scss/main.scss'
+</style>
---
+++ src/Editor.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="editor">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'editor',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
